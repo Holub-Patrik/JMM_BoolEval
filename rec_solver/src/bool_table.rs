63f45797e6d297329a072da284c3bbfb54447fb3
Holub-Patrik <23bulohp@gmail.com>
2023-11-20 17:06:39 +0100

    Rec Works!
    Now I need to add the ability for user defined operators and we're done

---
+++ rec_solver/src/bool_table.rs
@@ -0,0 +1,161 @@
+use std::fmt;
+use std::collections::HashMap;
+use regex::Regex;
+
+pub enum Operator {
+    AND,
+    OR,
+    EQ,
+    IMPL,
+}
+
+impl fmt::Display for Operator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Operator::AND => write!(f, "&&"),
+            Operator::OR => write!(f, "||"),
+            Operator::EQ => write!(f, "=="),
+            Operator::IMPL => write!(f, "=>"),
+        }
+    }
+    
+}
+
+pub struct BoolTable<'a> {
+    verbose: bool,
+    equation:  &'a str,
+    curr_int: u128,
+    tr_val_int: u128,
+    charset: HashMap<&'a str, Operator>,
+    var_to_indx: HashMap<&'a str, usize>,
+    re: Regex,
+    pub result_table: Vec<Vec<bool>>,
+}
+
+pub fn new<'a>(verbose:bool, equation:&'a str, tr_val_int:u128, 
+               charset:HashMap<&'a str, Operator>, 
+               var_to_indx:HashMap<&'a str, usize>, re:Regex) -> BoolTable<'a> {
+    BoolTable { 
+        verbose,
+        equation, 
+        curr_int: 0, 
+        tr_val_int, 
+        charset, 
+        var_to_indx, 
+        re, 
+        result_table: vec![]
+    }
+}
+
+impl BoolTable<'_> {
+    fn eval(&self, lhs:bool, rhs:bool, op:&Operator) -> bool {
+        match op {
+            Operator::AND => {
+                return lhs && rhs;
+            },
+            Operator::OR => {
+                return lhs || rhs;
+            },
+            Operator::EQ => {
+                return lhs == rhs;
+            },
+            Operator::IMPL => {
+                return lhs <= rhs;
+            }
+        }
+    }
+
+    fn str_eval(&self, lhs:&str, rhs:&str, op:&str) -> bool {
+        let mut b_lhs:bool;
+        let mut b_rhs:bool;
+         
+        if self.verbose {println!("str_eval(): Matching operator...");}
+        let b_op:&Operator = self.charset.get(op).unwrap();
+        if self.verbose {println!("str_eval(): Matched operator: {}", b_op);}
+
+        if lhs.contains("(") {
+            if self.verbose {println!("str_eval(): lhs impossible to evaluate, sending for breakdown");}
+            b_lhs = self.rec_eval(Some(lhs));
+            if lhs.contains("!") {
+                b_lhs = !b_lhs;
+            }
+        } else {
+            if lhs.contains("!") {
+                if self.verbose {println!("str_eval(not_lhs): Getting not value from {}...", lhs);}
+                b_lhs = !self.get_val(&lhs[1 .. ]);
+                if self.verbose {println!("str_eval(not_lhs): Got: {}", b_lhs);}
+            } else {
+                if self.verbose {println!("str_eval(normal_lhs): Getting value from {}...", lhs);}
+                b_lhs = self.get_val(&lhs);
+                if self.verbose {println!("str_eval(normal_lhs): Got: {}", b_lhs);}
+            }
+        }
+        
+        if rhs.contains("(") {
+            if self.verbose {println!("str_eval(): rhs impossible to evaluate, sending for breakdown");}
+            b_rhs = self.rec_eval(Some(rhs));
+            if rhs.contains("!") {
+                b_rhs = !b_rhs;
+            }
+        } else {
+            if rhs.contains("!") {
+                if self.verbose {println!("str_eval(not_rhs): Getting not value from {}...", rhs);}
+                b_rhs = !self.get_val(&rhs[1 .. ]);
+                if self.verbose {println!("str_eval(not_rhs): Got: {}", b_rhs);}
+            } else {
+                if self.verbose {println!("str_eval(normal_rhs): Getting value from {}...", rhs);}
+                b_rhs = self.get_val(&rhs);
+                if self.verbose {println!("str_eval(normal_rhs): Got: {}", b_rhs);}
+            }
+        }
+        
+        if self.verbose {println!("str_eval(): Evaluating...");}
+        let result:bool = self.eval(b_lhs, b_rhs, b_op);
+        if self.verbose {println!("str_eval(): Result: {}", result);}
+        return result;
+    }
+
+    fn get_val(&self, var:&str) -> bool {
+        let indx = self.var_to_indx.get(var).unwrap();
+        if self.verbose {println!("get_val(): Value: {} has the index: {} and I see binary: {:#010b}", var, indx, self.curr_int+self.tr_val_int);}
+        let val = (self.curr_int+self.tr_val_int)>>indx &1 == 1;
+        if self.verbose {println!("get_val(): Resulting value: {}", val);}
+        return val;
+    }
+
+    pub fn rec_eval(&self, equation:Option<&str>) -> bool {
+        let eq = equation.unwrap_or(self.equation);
+        if self.verbose {println!("rec_eval(): Evaluation: {} With int repr: {:#010b}", eq, self.curr_int);}
+        
+        let Some(parts) = self.re.captures(&eq) else {
+            if self.verbose {println!("invalid string!");}
+            return false;
+        };
+        if self.verbose {println!("rec_eval(): evaluting:{} {} {}",&parts["lhs"], &parts["rhs"], &parts["op"]);}
+        let answr = self.str_eval(&parts["lhs"], &parts["rhs"], &parts["op"]);
+        return answr;
+    }
+
+    pub fn create_table(&mut self) {
+        let complete_add_value:u128 = !self.tr_val_int;
+        let size = self.var_to_indx.len();
+
+        for _ in 0 .. 2_u128.pow(size.try_into().unwrap()) {
+            let mut line:Vec<bool> = vec![];
+            if complete_add_value | self.curr_int > complete_add_value {
+                self.curr_int += 1;
+                continue;
+            }
+            let actual_val = (complete_add_value & self.curr_int) + self.tr_val_int;
+            for i in 0 .. size {
+                let curr_val:bool = (actual_val>>i &1)==1;
+                line.push(curr_val);
+            }
+            if self.verbose {println!("----------Calling for recursive evaluation!----------");}
+            line.push(self.rec_eval(None));
+            self.result_table.push(line);
+            self.curr_int += 1;
+            if self.verbose {println!("----------Recursive Evaluation done----------\n");}
+        }
+    }
+}
--- rec_solver/src/main.rs
+++ rec_solver/src/main.rs
@@ -1,117 +1,169 @@
+use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::HashMap;
+mod bool_table;
+use bool_table::Operator;
+use std::env;
 
-enum Operator {
-    AND,
-    OR,
-    EQ,
-    IMPL,
+
+use std::io::Write;
+
+fn _bool_table(size:u32, true_val_repr:u128) -> () {
+    let stdout = std::io::stdout();
+    let lock = stdout.lock();
+    let mut w = std::io::BufWriter::new(lock);
+
+    let mut expr_val:u128 = 0;
+    let complete_add_value:u128 = !true_val_repr;
+
+    for _ in 0 .. 2_u128.pow(size.try_into().unwrap()) {
+        if complete_add_value | expr_val > complete_add_value {
+            expr_val+=1;
+            continue;
+        }
+        let actual_val = (complete_add_value & expr_val)+true_val_repr;
+        //println!("actual val: {} and expr_val = {}",actual_val, expr_val);
+        for i in 0 .. size {
+            let curr:u128 = actual_val >> i&1;
+            write!(&mut w, "{} ", curr.to_string()).unwrap();
+        }
+        writeln!(&mut w, "").unwrap();
+        expr_val+=1;
+        }
+    println!("");
 }
 
-struct BoolTable<'a> {
-    equation:  &'a str,
-    curr_int: u128,
-    tr_val_int: u128,
-    charset: HashMap<&'a str, Operator>,
-    re: Regex,
-}
-
-impl BoolTable<'_> {
-    fn eval(&self, lhs:bool, rhs:bool, op:&Operator) -> bool {
-        match op {
-            Operator::AND => {
-                return lhs && rhs;
-            },
-            Operator::OR => {
-                return lhs || rhs;
-            },
-            Operator::EQ => {
-                return lhs == rhs;
-            },
-            Operator::IMPL => {
-                return lhs <= rhs;
-            }
-        }
-    }
-
-    fn str_eval(&self, lhs:&str, rhs:&str, op:&str) -> bool {
-        let mut b_lhs:bool = false;
-        let mut b_rhs:bool = false;
-        
-        let b_op:&Operator = self.charset.get(op).unwrap();
-
-        if lhs.contains("(") {
-            b_lhs = self.rec_eval(Some(lhs));
-            if lhs.contains("!") {
-                b_lhs = !b_lhs;
-            }
-        } else {
-            if lhs.contains("!") {
-                b_lhs = !self.get_val(&lhs[1 .. ])
-            } else {
-                b_lhs = self.get_val(&lhs)
-            }
-        }
-        
-        if rhs.contains("(") {
-            b_rhs = self.rec_eval(Some(rhs));
-            if rhs.contains("!") {
-                b_rhs = !b_rhs;
-            }
-        } else {
-            if rhs.contains("!") {
-                b_rhs = !self.get_val(&rhs[1 .. ])
-            } else {
-                b_rhs = self.get_val(&rhs)
-            }
-        }
-
-        return self.eval(b_lhs, b_rhs, b_op)
-    }
-
-    fn get_val(&self, indx:&str) -> bool {
-        todo!()
-    }
-
-    fn rec_eval(&self, equation:Option<&str>) -> bool {
-        let eq = equation.unwrap_or(self.equation);
-        
-        let mut answr:bool = true;
-        let Some(parts) = self.re.captures(&eq) else {
-            println!("invalid string!");
-            return false;
-        };
-        answr = self.str_eval(&parts["lhs"], &parts["rhs"], &parts["op"]);
-        return answr;
-    }
-}
-
-fn pf<T>(_: &T) -> &'static str {
+fn _pf<T>(_: &T) -> &'static str {
     return std::any::type_name::<T>();
 }
 
+fn pretty_print(table:Vec<Vec<bool>>, vars:Vec<&str>, equ:&str) {
+    let mut max_width = 1;
+    let eq_width = equ.len();
+    
+    for var in &vars {
+        if var.len() > max_width { max_width = var.len() }
+    }
+    let total_len = vars.len()*3+vars.len()*max_width+eq_width+4;
+    println!("{:-^1$}", "Input", total_len);
+    let mut line = String::from("| ");
+    for var in &vars {
+        line.push_str(&format!("{:^1$} | ", var, max_width)); 
+    }
+    println!("{}{} |", line, equ);
+    println!("{:-^1$}", "Boolean Table", total_len);
+    for line in table {
+        let mut write_line = String::from("| ");
+        let mut peek_line = line.iter().peekable();
+        while let Some(val) = peek_line.next() {
+            let write_val = match val {
+                true => "T",
+                false => "F",
+            };
+            if peek_line.peek().is_none() {
+                write_line.push_str(&format!("{:^1$} | ", write_val, eq_width));
+            } else {
+                write_line.push_str(&format!("{:^1$} | ", write_val, max_width));
+            }
+        }
+        println!("{}", write_line)
+    }
+    println!("{:-<1$}", "", total_len);
+
+}
+
 fn main() {
-    let re = Regex::new(r"\((?<lhs>.+)(?<op>&&|==|\|\||=>)(?<rhs>.+)\)").unwrap();
-    let mut usr_in:String = String::new(); 
-    std::io::stdin().read_line(&mut usr_in).unwrap();
-    let trim_in = usr_in.trim();
-    
-    let Some(parts) = re.captures(&trim_in) else {todo!()}; 
-    println!("lhs: {} op: {} rhs: {}", &parts["lhs"], &parts["op"], &parts["rhs"]);
+    use std::time::Instant;
+    let args: Vec<String> = env::args().collect();
+    let verbose = args.contains(&String::from("-v"));
+    let performace = args.contains(&String::from("-p"));
+    lazy_static! {
+        static ref RE:Regex = Regex::new(r"\((?<lhs>!?[a-zA-Z0-9_-]+|!?\(.+\))(?<op>&&|==|\|\||=>)(?<rhs>!?[a-zA-Z0-9_-]+|!?\(.+\))\)").unwrap();
+        static ref VAR_RE:Regex = Regex::new(r"([a-zA-Z0-9_-]+)").unwrap();
+    }
+    let mut prep:Instant = Instant::now();
+    let mut eval:Instant = Instant::now();
+    println!("Please enter a valid boolean expression ");
+    loop {
+        print!(">> ");
+        std::io::stdout().flush().unwrap();
+        let mut usr_in:String = String::new(); 
+        std::io::stdin().read_line(&mut usr_in).unwrap();
+        
+        if performace {
+            println!("Preperation begins!");
+            prep = Instant::now();
+        }
+        //initialize the charset, eventually can be defined from file
+        let charset = HashMap::from([
+            ("&&", Operator::AND),
+            ("||", Operator::OR),
+            ("==", Operator::EQ),
+            ("=>", Operator::IMPL),
+        ]);
+        
+        //take user input
+        let trim_in = usr_in.trim();
+        
+        //get the equation and set vars
+        let (equ, tr_vars) = match trim_in.split_once(",") {
+            None => (trim_in, ""),
+            Some(tup) => tup,
+        };
 
+        if verbose {
+            println!("Found equation: {}", equ);
+            if tr_vars != "" {
+                println!("Found set true vals: {}", tr_vars);
+            }
+            let Some(parts) = RE.captures(&equ) else {todo!()}; 
+            println!("lhs: {} op: {} rhs: {}", &parts["lhs"], &parts["op"], &parts["rhs"]);
+        }
 
-    let charset = HashMap::from([
-        ("&&", Operator::AND),
-        ("||", Operator::OR),
-        ("==", Operator::EQ),
-        ("=>", Operator::IMPL),
-    ]);
+        let mut var_to_indx:HashMap<&str, usize> = HashMap::new();
+        let mut i:usize = 0;
+        let mut vars:Vec<&str> = vec![];
+        for (_,[var]) in VAR_RE.captures_iter(&equ).map(|c| c.extract()) {
+            //println!("Found var: {} and giving index: {}", var, i);
+            vars.push(var);
+            match var_to_indx.entry(var) {
+                std::collections::hash_map::Entry::Vacant(_) => {
+                    var_to_indx.insert(var, i);
+                    i += 1;
+                },
+                std::collections::hash_map::Entry::Occupied(_) => {},
+            }
+        }   
+
+        if verbose {println!("Var to index: {:?}", var_to_indx);}
+
+        let mut tr_val_int:u128 = 0;
+        for (_,[var]) in VAR_RE.captures_iter(&tr_vars).map(|c| c.extract()) {
+            let pow_val = match var_to_indx.get(var) {
+                Some(v) => v,
+                None => {println!("Incorect variable name when defining always true variables.\n Problematic name: {}", var);return;},
+            };
+            tr_val_int += 2_u128.pow(*pow_val as u32)
+        }
     
-    let answer = BoolTable {
-        equation: trim_in,
-        curr_int: 0,
-        tr_val_int: 0,
-        charset,
-        re,
-    };
+        if verbose {
+            let mut s = String::new();
+            std::io::stdin().read_line(&mut s).unwrap();
+        }
+        
+        let mut table_struct = bool_table::new(verbose, equ, tr_val_int, charset, var_to_indx, RE.to_owned());
+        if performace {
+            let prep_took = prep.elapsed();
+            println!("Preparations took: {:?}", prep_took);
+            println!("Evaluation begins!");
+            eval = Instant::now();
+        }
+        let _ = table_struct.create_table();
+        if performace {
+            let eval_took = eval.elapsed();
+            println!("Evaluation took: {:?}", eval_took);
+        }
+        if verbose {println!("Hopefully created the table!");}
+        pretty_print(table_struct.result_table, vars, equ)
+    }
 }
